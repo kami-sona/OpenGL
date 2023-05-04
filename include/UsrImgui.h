624f4e1531ae8306f0d16508893d1453e67a0153
ZhouCheng <zhouadmiku314@gmail.com>
2023-05-04 23:20:31 +0800

    add imgui module

---
+++ include/UsrImgui.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+
+class UsrImgui
+{
+public:
+	UsrImgui(GLFWwindow* winodw,const char*gl_version);
+	void Init();
+	void Update();
+	void Render();
+	void NewFrame();
+	void Shutdown();
+
+private:
+
+	GLFWwindow* cwindow;
+	const char* glsl_version;
+};
+
---
+++ src/UsrImgui.cpp
@@ -0,0 +1,49 @@
+#include "UsrImgui.h"
+
+UsrImgui::UsrImgui(GLFWwindow* window,const char* gl_version)
+{
+    cwindow = window;
+    glsl_version = gl_version;
+}
+
+void UsrImgui::Init()
+{
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(cwindow, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+}
+
+void UsrImgui::Update()
+{
+    ImGui::Begin("Hello, world!");                       
+    ImGui::Text("This is some useful text.");
+    ImGui::End();
+}
+
+void UsrImgui::Render()
+{
+    // Render dear imgui into screen
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void UsrImgui::NewFrame()
+{
+    // feed inputs to dear imgui, start new frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+}
+
+void UsrImgui::Shutdown()
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+}
